f019b487931d98c7496fb17cf48b0a89a823ceb7
abdullah <abdullahmusheer@gmail.com>
2025-07-30 10:16:29 -0300

    Added my API Dev Project

---
+++ My API Development Project/app/__init__.py
@@ -0,0 +1,6 @@
+## So here I am utlizing the dotenv package to secure my credentials on
+## inside a .env file and then use load_dotenv to actually grab credentials
+## from there.
+from dotenv import load_dotenv
+
+load_dotenv
---
+++ My API Development Project/main.py
@@ -0,0 +1,25 @@
+import requests
+from fastapi import FastAPI
+#calling the fastapi module - represents our entire application for Crud apps
+app = FastAPI()
+
+#now access this "app" created 
+
+@app.get("/hello")
+def hello():
+    return {"Hello":"how are you doing?"}
+
+@app.get("/funtest")
+def fun():
+    name =input ("whats your name? ")
+    return {f"thanks {name}"}
+
+@app.get("/")
+def root():
+    return {"this is the root page"}
+
+@app.get("/inquire")
+def inquire():
+    req = requests.get("https://catfact.ninja/fact")
+    return req.json()
+   
